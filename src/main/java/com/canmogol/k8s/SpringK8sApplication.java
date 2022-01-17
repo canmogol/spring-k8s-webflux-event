050240a15178b3c03721ac2fc405ce8e775c78b0
Can MOGOL <canmogol@gmail.com>
2022-01-17 16:54:11 +0100

    Remove unused annotation.
    Signed-off-by: Can MOGOL <canmogol@gmail.com>

--- src/main/java/com/canmogol/k8s/SpringK8sApplication.java
+++ src/main/java/com/canmogol/k8s/SpringK8sApplication.java
@@ -2,14 +2,12 @@ package com.canmogol.k8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringK8sApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringK8sApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringK8sApplication.class, args);
+    }
 
 }
