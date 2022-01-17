b63428cc26da8415b8a7ebd534c7d4e80015bfe0
Can MOGOL <canmogol@gmail.com>
2022-01-17 15:58:37 +0100

    Add spring k8s dependencies.
    Signed-off-by: Can MOGOL <canmogol@gmail.com>

--- src/main/java/com/canmogol/k8s/SpringK8sApplication.java
+++ src/main/java/com/canmogol/k8s/SpringK8sApplication.java
@@ -2,8 +2,10 @@ package com.canmogol.k8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringK8sApplication {
 
 	public static void main(String[] args) {
