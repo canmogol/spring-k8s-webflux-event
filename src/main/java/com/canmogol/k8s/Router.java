127aeab9e4b500f452231c0c2dff5c2cabd2cad5
Can MOGOL <canmogol@gmail.com>
2022-01-17 12:43:06 +0100

    Add configuration and a REST endpoint.
    Signed-off-by: Can MOGOL <canmogol@gmail.com>

---
+++ src/main/java/com/canmogol/k8s/Router.java
@@ -0,0 +1,41 @@
+package com.canmogol.k8s;
+
+import com.canmogol.k8s.config.AppConfig;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+/**
+ * Handles all the routes.
+ */
+@Configuration
+@AllArgsConstructor
+@Slf4j
+public class Router {
+
+    private final AppConfig config;
+
+    /**
+     * Entry point for all requests.
+     *
+     * @return Router function that handles all the requests.
+     */
+    @Bean
+    public RouterFunction<ServerResponse> all() {
+        return route(RequestPredicates.all(), this::handleRequest);
+    }
+
+    private Mono<ServerResponse> handleRequest(final ServerRequest request) {
+        return ServerResponse.ok().body(Mono.just(config.getKeyValuePairs()), Map.class);
+    }
+}
---
+++ src/main/java/com/canmogol/k8s/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.canmogol.k8s.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * Application level configuration.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "app.config")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppConfig {
+    private String logLevel = "info";
+    private Map<String, String> keyValuePairs;
+}
